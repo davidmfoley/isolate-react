631effa33a8a125d44b4c7ebe560856a3a8a3703
Dave Foley <davidmfoley@gmail.com>
2020-10-14 16:41:58 -0700

    Tests for matching empty selector

--- test/nodeMatcher_test.tsx
+++ test/nodeMatcher_test.tsx
@@ -37,6 +37,11 @@ const shouldNotMatchReact = (selector: string, fc: any, props: any) => {
 const Example = () => null
 
 describe('nodeMatcher', () => {
+  describe('empty matcher', () => {
+    shouldMatchHtmlNode('', 'div', { className: 'awesome' })
+    shouldMatchReact('', Example, {})
+  })
+
   describe('matching by class', () => {
     shouldMatchHtmlNode('.awesome', 'div', { className: 'awesome' })
     shouldMatchHtmlNode('span.awesome', 'span', { className: 'awesome' })
