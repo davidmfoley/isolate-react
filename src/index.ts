31865ee1e68daf28a1f6db4821abedb36a42cb49
Dave Foley <davidmfoley@gmail.com>
2020-10-13 22:43:12 -0700

    Move selector to nodeMatcher

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,7 @@
 import isolateHooks from 'isolate-hooks'
 import { nodeTree, NodeTree, TreeNode } from './nodeTree'
-import nodeMatcher from './nodeMatcher'
-export { TreeNode }
-
-/**
- * Spec for finding a child node of a component under test, used with `findOne` and `findAll`.
- *
- * Use a string to find html nodes using a subset of selector syntax:
- *
- * `div#awesome-id` and `#awesome-id` will find `<div id='awesome' />`
- * `span.cool` and `.cool` will each find `<span className='cool' />`m
- * `[data-test-id=foobar] will find the react element or html element with a `data-test-id` prop with the value `foobar`
- *
- * Use a component function  or name to find react components.
- *
- */
-export type Selector = string | React.FC<any>
+import nodeMatcher, { Selector } from './nodeMatcher'
+export { TreeNode, Selector }
 
 /**
  * Return value from isolateComponent.
--- src/nodeMatcher.ts
+++ src/nodeMatcher.ts
@@ -1,7 +1,20 @@
-import { FindSpec } from '.'
 import { TreeNode } from './nodeTree'
 
-const nodeMatcher = (spec: FindSpec | null): ((node: TreeNode) => boolean) => {
+/**
+ * Spec for finding a child node of a component under test, used with `findOne` and `findAll`.
+ *
+ * Use a string to find html nodes using a subset of selector syntax:
+ *
+ * `div#awesome-id` and `#awesome-id` will find `<div id='awesome' />`
+ * `span.cool` and `.cool` will each find `<span className='cool' />`m
+ * `[data-test-id=foobar] will find the react element or html element with a `data-test-id` prop with the value `foobar`
+ *
+ * Use a component function  or name to find react components.
+ *
+ */
+export type Selector = string | React.FC<any>
+
+const nodeMatcher = (spec: Selector | null): ((node: TreeNode) => boolean) => {
   if (!spec) return () => true
   if (typeof spec === 'string') {
     if (spec.includes('#')) {
