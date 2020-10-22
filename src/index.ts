70acdd4686caf5c5b0d88706d11217424d170652
Dave Foley <davidmfoley@gmail.com>
2020-10-22 06:01:51 -0700

    Fix prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-import isolateHooks from 'isolate-hooks'
-import { nodeTree, NodeTree } from './nodeTree'
 import { Selector } from './types/Selector'
 import { ComponentNode } from './types/ComponentNode'
 import { IsolatedComponent } from './types/IsolatedComponent'
 import { IsolateComponent } from './types/IsolateComponent'
 import { isolateComponent } from './isolateComponent'
+
 export { Selector, ComponentNode }
 export { IsolatedComponent, IsolateComponent, isolateComponent }
-
-type Contexts = { contextType: React.Context<any>; contextValue: any }[]
-
