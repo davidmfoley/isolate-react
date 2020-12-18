debaaa5671b2288a808d5bf945f99d869c2c4fbd
Dave Foley <davidmfoley@gmail.com>
2020-12-18 13:39:43 -0800

    Unused errors removed

--- src/errors.ts
+++
@@ -1,7 +0,0 @@
-export class ClassesNotSupportedError extends Error {
-  constructor() {
-    super(
-      'React class components are not yet supported. If this is a feature you need, please upvote or comment on the issue tracking this work: https://github.com/davidmfoley/isolate-components/issues/8'
-    )
-  }
-}
