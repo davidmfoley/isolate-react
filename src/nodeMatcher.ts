d744e2642193a108ee0d578ab684ee96bc5b6557
Dave Foley <davidmfoley@gmail.com>
2021-09-26 11:19:21 -0700

    Clean up nodeMatcher

--- src/nodeMatcher.ts
+++ src/nodeMatcher.ts
@@ -1,30 +1,45 @@
 import { Selector } from './types/Selector'
 import { TreeNode } from './types/TreeNode'
 
+const idMatcher = (spec: string): NodeMatcher => {
+  const [tag, id] = spec.split('#')
+  return (node) => (!tag || tag === node.name) && id === node.props.id
+}
+
+const classOrExactMatcher = (spec: string): NodeMatcher => {
+  const [tag, className] = spec.split('.')
+  return (node) =>
+    ((!tag || tag === node.name) &&
+      node.props.className.split(' ').includes(className)) ||
+    spec === node.name
+}
+
+const propMatcher = (spec: string): NodeMatcher => {
+  const [name, rest] = spec.split('[')
+  const [key, value] = rest.split(']')[0].split('=')
+  return (node) => (!name || name === node.name) && node.props[key] === value
+}
+
+const nameMatcher = (spec: Selector): NodeMatcher => {
+  return (node) => node.type === spec || node.name === spec
+}
+
 export const nodeMatcher = (spec: Selector | null | undefined): NodeMatcher => {
   if (!spec || spec === '*') return () => true
   if (typeof spec === 'string') {
     if (spec.includes('#')) {
-      const [tag, id] = spec.split('#')
-      return (node) => (!tag || tag === node.name) && id === node.props.id
+      return idMatcher(spec)
     }
     if (spec.includes('.')) {
-      const [tag, className] = spec.split('.')
-      return (node) =>
-        ((!tag || tag === node.name) &&
-          node.props.className.split(' ').includes(className)) ||
-        spec === node.name
+      return classOrExactMatcher(spec)
     }
 
     if (/\[.+\]/.test(spec)) {
-      const [name, rest] = spec.split('[')
-      const [key, value] = rest.split(']')[0].split('=')
-      return (node) =>
-        (!name || name === node.name) && node.props[key] === value
+      return propMatcher(spec)
     }
   }
 
-  return (node) => node.type === spec || node.name === spec
+  return nameMatcher(spec)
 }
 
 export default nodeMatcher
