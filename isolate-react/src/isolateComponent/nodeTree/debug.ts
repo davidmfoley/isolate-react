0147bb5ed9967c6df37bf451ca8a83d943e036a9
Dave Foley <davidmfoley@gmail.com>
2022-07-28 09:35:03 -0700

    Handle circular ref in debug()

--- isolate-react/src/isolateComponent/nodeTree/debug.ts
+++ isolate-react/src/isolateComponent/nodeTree/debug.ts
@@ -1,5 +1,19 @@
 import { TreeNode } from '../types'
 
+const debugType = (type: any) => {
+  if (typeof type === 'function') return { type: type.name }
+  if (type._context) {
+    return {
+      type:
+        type._context.Consumer === type
+          ? `context consumer`
+          : `context provider`,
+      value: type._context._currentValue,
+    }
+  }
+  return { type }
+}
+
 export const doDebug = (node: TreeNode, indent = 0) => {
   let lines = [] as string[]
   const space = indent
@@ -9,14 +23,12 @@ export const doDebug = (node: TreeNode, indent = 0) => {
     : ''
   let pushLine = (s: string) => lines.push(`${space}${s}`)
 
-  const { children, ...rest } = node
+  const { children, componentInstance, type, ...rest } = node
 
-  pushLine(JSON.stringify(rest))
+  pushLine(JSON.stringify({ ...rest, ...debugType(type) }))
 
   if (node.nodeType === 'isolated') {
-    lines = lines.concat(
-      doDebug(node.componentInstance!.tree().root(), indent + 1)
-    )
+    lines = lines.concat(doDebug(componentInstance!.tree().root(), indent + 1))
   } else {
     node.children.forEach((child) => {
       lines = lines.concat(doDebug(child, indent + 1))
