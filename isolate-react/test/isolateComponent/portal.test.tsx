c17cc48a658ef0149bbe95e2fb973b8615935386
Dave Foley <davidmfoley@gmail.com>
2022-10-17 08:17:17 -0700

    Test for inline inside portal, basic portal documentation.

--- isolate-react/test/isolateComponent/portal.test.tsx
+++ isolate-react/test/isolateComponent/portal.test.tsx
@@ -4,16 +4,32 @@ import ReactDOM from 'react-dom'
 import { isolateComponent } from '../../src/isolateComponent'
 import assert from 'node:assert'
 
-describe('portals', () => {
-  test('renders portal content', () => {
-    const element = { nodeType: 1 }
+// https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+const ELEMENT_NODE = 1
 
-    const ExamplePortalizer = () => {
-      return ReactDOM.createPortal(<div />, element as any)
-    }
+describe('portals', () => {
+  // createPortal accepts a DOM Node, this is a partial
+  // implementation of just enough to fool react's guard
+  const fakePortalDomElement = { nodeType: ELEMENT_NODE } as any
+
+  test('render portal content', () => {
+    const ExamplePortalizer = () =>
+      ReactDOM.createPortal(<div />, fakePortalDomElement as any)
 
     const isolated = isolateComponent(<ExamplePortalizer />)
 
     assert.strictEqual(isolated.toString(), '<div />')
+  })
+
+  test('inline component inside portal', () => {
+    const Hello = ({ name }) => <span>Hello {name}</span>
+
+    const ComponentInsidePortal = ({ name }) =>
+      ReactDOM.createPortal(<Hello name={name} />, fakePortalDomElement as any)
+
+    const isolated = isolateComponent(<ComponentInsidePortal name="Arthur" />)
+    isolated.inline(Hello)
+
+    assert.strictEqual(isolated.toString(), '<span>Hello Arthur</span>')
   })
 })
